b654890cb045b04d3d9eec3e5a38d4c6237114cf
mioe <mioe.boss@gmail.com>
2022-10-16 19:50:24 +0300

    init

---
+++ src/build.ts
@@ -0,0 +1,10 @@
+/**
+ * Styles
+ */
+import 'uno.css'
+import './style.css'
+
+/**
+ * Export components
+ */
+export { default } from './components/HelloWorld.vue'
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import unocss from 'unocss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		vue(),
+
+		// https://github.com/antfu/unocss
+		unocss(),
+	],
+
+	build: {
+		lib: {
+			entry: `${resolve(__dirname, 'src')}/build.ts`,
+			name: 'vite-ts-build-template',
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ['vue'],
+			output: {
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals: {
+					vue: 'Vue',
+				},
+			},
+		},
+	},
+})
